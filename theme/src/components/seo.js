762dc47ca896161dd5c0cdf81d123898b709d803
Cody Hatfield <cody.hatfield@me.com>
2021-08-26 16:49:39 -0700

    Add favicon

--- theme/src/components/seo.js
+++ theme/src/components/seo.js
@@ -31,6 +31,26 @@ export default function SEO({ title, description, children }) {
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={seo.author} />
       <meta name="gatsby-theme" content="gatsby-theme-cactus" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon-light.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-light-32x32.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-light-16x16.png"
+        media="(prefers-color-scheme: light)"
+      />
       {children}
     </Helmet>
   );
