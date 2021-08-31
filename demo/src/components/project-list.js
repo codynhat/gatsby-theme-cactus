f86fd74c314fce40552d0a9892e2a64cc7733dfb
Cody Hatfield <cody.hatfield@me.com>
2021-08-31 14:41:51 -0700

    Fix project list link

--- demo/src/components/project-list.js
+++ demo/src/components/project-list.js
@@ -6,7 +6,7 @@ import { Underline } from "@codynhat/gatsby-theme-cactus/src/components";
 import formatTime from "@codynhat/gatsby-theme-cactus/utils/format-time";
 
 export default function ProjectList() {
-  const { allNote } = useStaticQuery(ProjectListQuery);
+  const { allMdx } = useStaticQuery(ProjectListQuery);
 
   return (
     <section>
@@ -14,12 +14,15 @@ export default function ProjectList() {
         <h2 sx={{ variant: `title` }}>Projects</h2>
       </Link>
       <Styled.ul>
-        {allNote.edges.map(({ node }) => {
+        {allMdx.edges.map(({ node }) => {
           return (
             <li key={node.id} sx={{ mb: 2 }}>
               <Underline themeColor="text" hoverThemeColor="secondary">
-                <Link to={`${node.slug}`} sx={{ variant: `links.underline` }}>
-                  {node.title}
+                <Link
+                  to={`${node.frontmatter.slug ?? node.slug}`}
+                  sx={{ variant: `links.underline` }}
+                >
+                  {node.frontmatter.title}
                 </Link>
               </Underline>
               : {node.excerpt}
@@ -33,18 +36,21 @@ export default function ProjectList() {
 
 const ProjectListQuery = graphql`
   query {
-    allNote(
-      sort: { fields: [date, title], order: DESC }
-      filter: { tags: { in: "project" } }
+    allMdx(
+      sort: { fields: [frontmatter___date, frontmatter___title], order: DESC }
+      filter: { frontmatter: { tags: { in: "project" } } }
     ) {
       edges {
         node {
           id
           excerpt
           slug
-          title
-          date(formatString: "DD MMM YYYY")
-          tags
+          frontmatter {
+            title
+            slug
+            date(formatString: "DD MMMM, YYYY")
+            link
+          }
         }
       }
     }
